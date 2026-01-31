b6f093d210f6111b857c79aeaf9ca74669879a8d
mitzvetsia <35780481+mitzvetsia@users.noreply.github.com>
2026-01-30 21:26:18 -0500

    Create hide-navigation.js

---
+++ src/features/hide-navigation.js
@@ -0,0 +1,73 @@
+
+
+// PM REVIEW SYSTEM QUESTIONNAIRE
+$(document).on('knack-scene-render.scene_1003', function (event, scene) {
+//$('.kn-back-link').hide();
+$(".kn-crumbtrail").hide();
+//$(".kn-title").hide();
+$(".kn-info-bar").hide();
+//$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+  $(".kn-menu.view_44").hide();
+});
+
+
+
+
+
+// NEW Q1 2024 Technician SOW View
+$(document).on('knack-scene-render.scene_915', function (event, scene) {
+$('.kn-back-link').hide();
+$(".kn-crumbtrail").hide();
+$(".kn-title").hide();
+$(".kn-info-bar").hide();
+$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+  $(".kn-menu.view_44").hide();
+});
+
+
+
+
+// NEW Q2 2023 Customer Quote View-Hide all options to navigate except for print
+$(document).on('knack-scene-render.scene_828', function (event, scene) {
+$('.kn-back-link').hide();
+$(".kn-crumbtrail").hide();
+$(".kn-title").hide();
+$(".kn-info-bar").hide();
+$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+  $(".kn-menu.view_44").hide();
+});
+
+// NEW Q2 2023 Scope of Work-Hide all options to navigate except for print
+$(document).on('knack-scene-render.scene_833', function (event, scene) {
+$('.kn-back-link').hide();
+$(".kn-crumbtrail").hide();
+$(".kn-title").hide();
+$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+$(".kn-menu.view_44").hide();
+});
+
+// NEW Q3 2023 DRAFT Quote View-Hide all options to navigate except for print
+$(document).on('knack-scene-render.scene_873', function (event, scene) {
+$('.kn-back-link').hide();
+$(".kn-crumbtrail").hide();
+//$(".kn-title").hide();
+$(".kn-info-bar").hide();
+$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+  $(".kn-menu.view_44").hide();
+});
+
+// NEW Q2 2023 DRAFT Scope of Work-Hide all options to navigate except for print
+$(document).on('knack-scene-render.scene_886', function (event, scene) {
+$(".kn-back-link").hide();
+$(".kn-crumbtrail").hide();
+//$(".kn-title").hide();
+$(".kn-info-bar").hide();
+$(".kn-app-header").hide();
+$(".kn-navigation-bar").hide();
+$(".kn-menu.view_44").hide();
+});
