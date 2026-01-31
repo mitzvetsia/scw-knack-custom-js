99b3e627c8f19ee0af1dcd75590c8301de243526
mitzvetsia <35780481+mitzvetsia@users.noreply.github.com>
2026-01-30 21:54:28 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cat \
   src/config.js \
   src/util.js \
   src/features/ratking.js \
-  src/features/proposals.js \
+  src/features/proposal-grid.js \
   src/features/group-collapse.js \
   src/features/add-sow-line-item-dto-hide-show-fields.js \
   src/features/highlight-duplicate-cells.js \
@@ -15,12 +15,12 @@ cat \
   src/features/change-record-limit.js \
   src/features/lock-fields.js \
   src/features/truncate-expand-function.js \
-  src/features/legacy/checkbox-grid.field_739.js \
   src/features/sync-checkboxes \
   src/features/survey-form-drag-drop-files.js \
   src/features/hide-navigation.js \
+  src/features/calc-install-fee-adjustment.js \
+  src/features/legacy/checkbox-grid.field_739.js \
   src/features/legacy/expand-collapse-legacy-function.js \
-  src/features/calc-install-fee-adjustment.js
   > dist/knack-bundle.js
 
 echo "✔ Built dist/knack-bundle.js"
