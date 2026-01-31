0eaaceff29ed71953c780aca8be46cb136e9b79f
mitzvetsia <35780481+mitzvetsia@users.noreply.github.com>
2026-01-30 21:49:56 -0500

    Create calc-install-fee-adjustment.js

---
+++ src/features/calc-install-fee-adjustment.js
@@ -0,0 +1,65 @@
+/**************************************************************************************************
+ * FEATURE: McGandy’s Experiment (scene_213) — margin/cost math
+ * ⚠ Contains likely bug: document.querySelector('text#field_1365') should probably be input#field_1365
+ **************************************************************************************************/
+(function mcgandyExperiment_scene213() {
+  $(document).on('knack-scene-render.scene_213', function (event, view, record) {
+    setTimeout(function () {
+
+      // subcontractor cost changed
+      $('input#field_1364').change(function () {
+        var subcontractor_cost = $(this).val();
+        var survey_cost = document.querySelector('input#field_1363').value;
+        var total_cost = Math.abs(Math.abs(survey_cost) + Math.abs(subcontractor_cost));
+        var margin = document.querySelector('input#field_1365').value;
+        var marked_up_labor = Math.round(total_cost / (1 - margin));
+
+        var install_total = Knack.models['view_507'].toJSON().field_343.replaceAll(',', '').replaceAll('$', '');
+        var fees_added = document.querySelector('input#field_1251').value.replaceAll(',', '');
+        var more_fees_to_add = Math.round((marked_up_labor - install_total) + Math.round(fees_added));
+
+        $('input#field_1366').val(marked_up_labor);
+        $('input#field_1580').val(more_fees_to_add);
+      });
+
+      // survey cost changed
+      $('input#field_1363').change(function () {
+        var survey_cost = $(this).val();
+        var subcontractor_cost = document.querySelector('input#field_1364').value;
+        var total_cost = Math.abs(Math.abs(survey_cost) + Math.abs(subcontractor_cost));
+        var margin = document.querySelector('text#field_1365').value; // ⚠ likely wrong selector
+        var marked_up_labor = Math.round(total_cost / (1 - margin));
+
+        $('input#field_1366').val(marked_up_labor);
+        $('input#field_1365').keyup();
+      });
+
+      // marked up labor changed -> update margin
+      $('input#field_1366').change(function () {
+        var marked_up_labor = $(this).val();
+        var survey_cost = document.querySelector('input#field_1363').value;
+        var subcontractor_cost = document.querySelector('input#field_1364').value;
+        var total_cost = Math.abs(Math.abs(survey_cost) + Math.abs(subcontractor_cost));
+        var margin = Math.abs(Math.round(marked_up_labor - total_cost) / marked_up_labor);
+        var margin_rounded = Math.round((margin + Number.EPSILON) * 100) / 100;
+
+        $('input#field_1365').val(margin_rounded);
+        $('input#field_1365').keyup();
+      });
+
+      // margin changed -> update marked up labor
+      $('input#field_1365').change(function () {
+        var margin = $(this).val();
+        var survey_cost = document.querySelector('input#field_1363').value;
+        var subcontractor_cost = document.querySelector('input#field_1364').value;
+        var total_cost = Math.abs(Math.abs(survey_cost) + Math.abs(subcontractor_cost));
+        var marked_up_labor = Math.round(total_cost / (1 - margin));
+
+        $('input#field_1366').val(marked_up_labor);
+        $('input#field_1366').keyup();
+      });
+
+    }, 1);
+  });
+})();
+/*** END FEATURE: McGandy’s Experiment ********************************************************************/
