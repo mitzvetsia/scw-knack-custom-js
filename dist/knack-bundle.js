3d32b25f219511b844d2b43c549c0c3a59cbd701
mitzvetsia <35780481+mitzvetsia@users.noreply.github.com>
2026-01-30 20:00:15 -0500

    Update knack-bundle.js

--- dist/knack-bundle.js
+++ dist/knack-bundle.js
@@ -1,12 +1,4 @@
 
- KnackInitAsync = function ($, callback) { // Load the Knack Toolkit Library (KTL)
-    (window.LazyLoad = LazyLoad) && LazyLoad.js(['https://ctrnd.s3.amazonaws.com/Lib/KTL/KTL_Start.js'], () => {
-        loadKtl($, callback, (typeof KnackApp === 'function' ? KnackApp : null), '' /*KTL version, leave blank to get latest*/, 'full'/*min or full*/ );
-    })
-};
-
-
-
 
 
 
