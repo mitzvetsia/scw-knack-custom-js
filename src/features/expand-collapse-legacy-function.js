b00ffe9a4e5734cec81b28fa2e77ba294635fdb6
mitzvetsia <35780481+mitzvetsia@users.noreply.github.com>
2026-01-30 21:47:07 -0500

    Create expand-collapse-legacy-function.js

---
+++ src/features/expand-collapse-legacy-function.js
@@ -0,0 +1,127 @@
+/**************************************************************************************************
+ * FEATURE: “Scene section” expand/collapse (legacy)
+ * - Toggles view sections using .view-header as a clickable accordion
+ **************************************************************************************************/
+
+/** BINDINGS: call addSceneExpandCollapse(view) on view render */
+(function bind_sceneExpandCollapse() {
+  // Admin
+  $(document).on('knack-view-render.view_1218', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Micah's Shit
+  $(document).on('knack-view-render.view_1190', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1584', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1559', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1380', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_760',  function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1212', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_462',  function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1049', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1314', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Project Dashboard
+  $(document).on('knack-view-render.view_1224', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1498', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Job Reports (AVL / TRIAD)
+  $(document).on('knack-view-render.view_845',  function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1231', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1257', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1420', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1392', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1418', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Job Reports > In Work
+  $(document).on('knack-view-render.view_1302', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1309', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Service Calls & Troubleshooting
+  $(document).on('knack-view-render.view_1361', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1411', function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Project Summary
+  $(document).on('knack-view-render.view_1185', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1368', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_1710', function (event, view, data) { addSceneExpandCollapse(view); });
+  $(document).on('knack-view-render.view_899',  function (event, view, data) { addSceneExpandCollapse(view); });
+
+  // Build Quote
+  $(document).on('knack-view-render.view_2812', function (event, view, data) { addSceneExpandCollapse(view); });
+
+})();
+
+/** IMPLEMENTATION: addSceneExpandCollapse(view) */
+var addSceneExpandCollapse = function (view) {
+  $('#' + view.key + ' .view-header').css("cursor", "pointer");
+
+  $('#' + view.key + " .view-header h2").each(function () {
+    if ($(this).text().length > 1) {
+      var RowText = $(this).html();
+      $(this).html('<i class="fa fa-plus-square-o";></i>&nbsp;' + RowText);
+    }
+  });
+
+  // Collapse by default
+  $('#' + view.key + ' .view-header').nextUntil('.view-header').toggle();
+
+  // Toggle on click (+/-)
+  $('#' + view.key + ' .view-header').click(function () {
+    $(this).nextUntil('.view-header').toggle();
+
+    if ($(this).html().indexOf('fa-plus') !== -1) {
+      $(this).html($(this).html().replace('plus', 'minus'));
+    } else {
+      $(this).html($(this).html().replace('minus', 'plus'));
+    }
+  });
+};
+
+/** DUPLICATE/ALT IMPLEMENTATION (kept for legacy reasons)
+ * - addSceneExpandCollapseMultiple is functionally the same as addSceneExpandCollapse
+ */
+var addSceneExpandCollapseMultiple = function (view) {
+  $('#' + view.key + ' .view-header').css("cursor", "pointer");
+
+  $('#' + view.key + " .view-header h2").each(function () {
+    if ($(this).text().length > 1) {
+      var RowText = $(this).html();
+      $(this).html('<i class="fa fa-plus-square-o"; ></i>&nbsp;' + RowText);
+    }
+  });
+
+  $('#' + view.key + ' .view-header').nextUntil('.view-header').toggle();
+
+  $('#' + view.key + ' .view-header').click(function () {
+    $(this).nextUntil('.view-header').toggle();
+
+    if ($(this).html().indexOf('fa-plus') !== -1) {
+      $(this).html($(this).html().replace('plus', 'minus'));
+    } else {
+      $(this).html($(this).html().replace('minus', 'plus'));
+    }
+  });
+};
+/*** END FEATURE: “Scene section” expand/collapse *********************************************************/
+
+
+/**************************************************************************************************
+ * FEATURE: Add row selection checkboxes to a table (utility)
+ * ⚠ NOTE: This function is defined AGAIN later in your original blob.
+ * Keeping one canonical copy is strongly recommended.
+ **************************************************************************************************/
+var addCheckboxes = function (view) {
+  // add checkbox in header (select/unselect all)
+  $('#' + view.key + '.kn-table thead tr').prepend('<th><input type="checkbox"></th>');
+
+  $('#' + view.key + '.kn-table thead input').change(function () {
+    $('.' + view.key + '.kn-table tbody tr input').each(function () {
+      $(this).attr('checked', $('#' + view.key + '.kn-table thead input').attr('checked') != undefined);
+    });
+  });
+
+  // add checkbox to each row in the table body
+  $('#' + view.key + '.kn-table tbody tr').each(function () {
+    $(this).prepend('<td><input type="checkbox"></td>');
+  });
+};
+/*** END FEATURE: Add row selection checkboxes ************************************************************/
