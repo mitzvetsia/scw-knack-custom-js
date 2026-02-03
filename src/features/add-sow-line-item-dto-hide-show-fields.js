e286272cbe72b21499610ef3f5118da01dabddfa
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-03 10:24:02 -0500

    fixing typo

--- src/features/add-sow-line-item-dto-hide-show-fields.js
+++ src/features/add-sow-line-item-dto-hide-show-fields.js
@@ -66,7 +66,7 @@
   };
 
   const ALL_FIELD_KEYS = [
-    'field_2182','field_2180','field_2188','field_2193','field_2194','field_2183','field_2210','field_2224','field_2248','field_2250'
+    'field_2182','field_2180','field_2188','field_2193','field_2194','field_2183','field_2210','field_2224','field_2248','field_2250',
     'field_2206','field_2195','field_2241','field_2184','field_2186','field_2187','field_2204', 'field_2211','field_2233','field_2246',
   ];
 
