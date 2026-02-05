ce2d3502a125c598779381a94458e13bed5d8ff0
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-05 18:47:49 -0500

    sdfsdf

--- src/features/proposal-grid.js
+++ src/features/proposal-grid.js
@@ -1575,7 +1575,7 @@ ${sceneSelectors} .kn-table-group.kn-group-level-4 td:first-child {padding-left:
 
   // ============================================================
   // EVENT BINDING (multi-view)
-  // ==========================================================
+  // ============================================================
 
   function bindForView(viewId) {
     const ev = `knack-records-render.${viewId}${CONFIG.eventNs}`;
