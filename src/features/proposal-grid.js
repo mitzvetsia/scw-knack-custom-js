b07771aa13fca47ff76a18d9c241f7de51491f49
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-05 23:35:12 -0500

    sdfsF

--- src/features/proposal-grid.js
+++ src/features/proposal-grid.js
@@ -524,7 +524,7 @@ tr.scw-level-total-row.scw-subtotal--level-1.scw-l1-line--final .scw-l1-value{
 
 /* 80px whitespace ABOVE the first L1 footer row */
 tr.scw-level-total-row.scw-subtotal--level-1.scw-l1-first-row td{
-  border-top: 80px solid transparent !important;
+  border-top: 40px solid transparent !important;
   Border-bottom: 5px solid #07467c !important;
 }
 
