2f843d5aa7540c36cd8401b38039ec133b6f7e05
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-10 12:34:34 -0500

    tweaking CSS

--- src/features/proposal-grid.js
+++ src/features/proposal-grid.js
@@ -710,7 +710,7 @@ ${sceneSelectors} .kn-table-group.kn-group-level-4 td:first-child {padding-left:
 
 /* Connected Devices on L3 headers */
 .scw-l3-connected-br { line-height: 0; }
-.scw-l3-connected-devices { display: inline-block; margin-top: 2px; line-height: 1.2; font-size: 14px; font-weight: 400; color: orange; }
+.scw-l3-connected-devices { display: inline-block; margin-top: 0px; line-height: 1; font-size: 14px; font-weight: 600; color: orange; }
 /********************* LEVEL 4 (INSTALL DESCRIPTION) ***********************/
 `;
 
