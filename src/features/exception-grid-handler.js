b87ecff6e4548a53d39c411d3e6d305c56f566cb
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-09 23:05:35 -0500

    sdfsdf

--- src/features/exception-grid-handler.js
+++ src/features/exception-grid-handler.js
@@ -33,7 +33,8 @@
         display: flex !important;
         align-items: center !important;
         justify-content: flex-start !important;
-	size: 12px !important;
+	font-size: 12px !important;
+	font-weight: 400 !important;
 
         width: 100% !important;
         background-color: ${WARNING_BG} !important;
