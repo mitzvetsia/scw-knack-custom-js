ac7b6c3b5cfb15f895fdc78236d3cabb86607f37
Mizvetsia <micah@security-camera-warehouse.com>
2026-02-10 14:13:12 -0500

    Release v1.1.238

--- dist/knack-bundle.js
+++ dist/knack-bundle.js
@@ -37,7 +37,7 @@ window.SCW = window.SCW || {};
       color: #07467c !important;
       font-size: 24px !important;
       margin-bottom: -15px !important;
-      margin-top: 15px !important;
+      margin-top: 30px !important;
     }
 
     /* KTL hide/show (shrink) button */
