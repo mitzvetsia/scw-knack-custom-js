8a24cc6dce5bb596e1a8ca8aadc8c10457bf73fc
Mizvetsia <micah@security-camera-warehouse.com>
2026-03-05 21:36:38 -0500

    Release v1.1.315

--- dist/knack-bundle.js
+++ dist/knack-bundle.js
@@ -533,7 +533,7 @@ window.SCW = window.SCW || {};
     attributeFilter: ['class'],
   });
 })();
-/***************  Extract _hsvcolor keyword from view descriptions  **********/
+/*****************  Extract _hsvcolor keyword from view descriptions  **********/
 (function () {
   'use strict';
 
